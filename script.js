2832eba84b3aa7328d5c55a2f7a241f5e3670ad5
Valentin <wallentin30@yahoo.com>
2020-03-18 21:32:07 +0200

    know it works :D

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const lastName = document.querySelector('#lastName');
 const userMessage = document.querySelector('#userMessage');
 const gender = document.getElementsByName("gender");
 
+
 firstName.addEventListener('blur', (e) => {
     if(firstName.value === "") {
         e.target.style.border = '1px solid red';
@@ -37,26 +38,28 @@ userMessage.addEventListener('blur', (e) => {
     }
 })
 
-function succesMessage() {
-    const succesMessage = document.querySelector('#succesMessage');
-    succesMessage.innerHTML = ` <i class="fas fa-check-circle"></i>
-        Thank you for contacting us, ${firstName.value} <span id="close">&#10005;</span>`;
-    succesMessage.style.visibility = "visible";
+    function succesMessage() {
+        const succesMessage = document.querySelector('#succesMessage');
 
-    document.querySelector("#close").addEventListener('click', function() {
+        if(firstName.value ,lastName.value,userMessage.value === "") {
+                alert('Please fill the red border inputs!');       
+        }else{
+            succesMessage.innerHTML = ` <i class="fas fa-check-circle"></i>
+                Thank you for contacting us, ${firstName.value} <span id="close">&#10005;</span>`;
+            succesMessage.style.visibility = "visible";
+
+            document.querySelector("#close").addEventListener('click', function() {
             document.querySelector("#succesMessage").style.visibility = "hidden";    
-        })
-    
-        for(let i=0; i < gender.length; i++) {
-            if(gender[i].checked) {
-                console.log(` Contact Form Response \n 
-                First Name: ${firstName.value} \n 
-                Last Name: ${lastName.value} \n
-                Gender: ${gender[i].value} \n
-                Message: ${userMessage.value}`);        
+            })
+            for(let i=0; i < gender.length; i++) {
+                if(gender[i].checked) {
+                    console.log(` Contact Form Response \n 
+                    First Name: ${firstName.value} \n 
+                    Last Name: ${lastName.value} \n
+                    Gender: ${gender[i].value} \n
+                    Message: ${userMessage.value}`);        
+                    }
+                }
             }
-        }
-        
 
-    
 }
